d2c91f7211c4cd7b34abbbec45db52eee0b42158
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2024-06-09 19:20:43 +1200

    docs: add comments

--- Assets/Scripts/Managers/MainManager.cs
+++ Assets/Scripts/Managers/MainManager.cs
@@ -1,15 +1,29 @@
+/// <remarks>
+/// Author: Johnathan
+/// Date Created: 7/6/2024
+/// Contributors: Assisted by Github Copilot
+/// </remarks>
+// <summary>
+/// This class controls the game state
+/// </summary>
+
 using System.Collections;
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEditor.PackageManager.Requests;
 
+/// <summary>
+/// This class controls the game state
+/// </summary>
 public class MainManager : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private MoveCamera moveCamera;
     [SerializeField] private UIManager uIManager;
-    [SerializeField] private PlayerController player;
     [SerializeField] private MeterManager meterManager;
+    [SerializeField] private PlayerController player;
+
+    [Header("Settings")]
     [SerializeField] private float timeBeforeReset = 2f;
     public bool GameStarted { get; private set; } = false;
 
@@ -31,35 +45,33 @@ public class MainManager : MonoBehaviour
 
     void Start()
     {
-        // consume ReachedPoleEvent from PlayerController
-        //player.ReachedPoleEvent.AddListener(() => balanceBeam?.StartBalanceBeam());
         player.PlayerFellEvent.AddListener(EndGame);
         meterManager.ActivateGameObjects(false);
     }
 
+    /// <summary>
+    /// Ends the game
+    /// </summary>
     private void EndGame()
     {
         meterManager.StopMeters();
         StartCoroutine(WaitThen(Reset));
     }
 
+    /// <summary>
+    /// Waits for a certain amount of time before calling a callback
+    /// </summary>
+    /// <param name="callback">The callback to call after the time has passed</param>
     private IEnumerator WaitThen(Action callback)
     {
         yield return new WaitForSeconds(timeBeforeReset);
         callback?.Invoke();
     }
 
-    private IEnumerator WaitThen(Action[] callbacks)
-    {
-        yield return new WaitForSeconds(timeBeforeReset);
-
-        foreach (var callback in callbacks)
-        {
-            callback?.Invoke();
-        }
-    }
-
-    // called when any key is pressed
+    /// <summary>
+    /// Starts the game
+    /// </summary>
+    /// <param name="context">The context of the input action</param>
     public void OnAnyKeyPressed(InputAction.CallbackContext context)
     {
         if (GameStarted == false)
@@ -68,6 +80,10 @@ public class MainManager : MonoBehaviour
             moveCamera.MoveToGamePosition(StartFromTitleScreen);
         }
     }
+
+    /// <summary>
+    /// Starts the game from the title screen
+    /// </summary>
     public void StartFromTitleScreen()
     {
         GameStarted = true;
@@ -75,6 +91,9 @@ public class MainManager : MonoBehaviour
         meterManager.StartRunningSlider();
     }
 
+    /// <summary>
+    /// Resets the game
+    /// </summary>
     private void Reset()
     {
         player.Reset();
