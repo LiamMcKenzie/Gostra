86fc8afb4fb71a5e3483ed1a80648d21a7f34a96
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2024-06-14 12:31:44 +1200

    feat: add god mode to player controller

--- Assets/Scripts/PlayerMovement/PlayerController.cs
+++ Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -18,7 +18,7 @@ using UnityEngine.Events;
 public class PlayerController : MonoBehaviour
 {
     [HideInInspector] public UnityEvent ReachedPoleEvent;
-    [HideInInspector] public UnityEvent PlayerFellEvent;
+    [HideInInspector] public UnityEvent<float> PlayerFellEvent;
 
     [Header("Components")]
     [SerializeField] private Animator animator;
@@ -27,25 +27,31 @@ public class PlayerController : MonoBehaviour
 
     [Header("Speed")]
     [SerializeField] private float speedReductionFactor = 0.5f; // The factor to reduce speed by
-    [field: SerializeField] public float Speed { get; set; } = 1f;  // The speed at which to move the object
 
     [Header("Rotation")]
     [SerializeField, Range(-1, 1)] public float playerRotation; //-1 and 1 are the limits for player rotation, if rotation is over 1 or less than -1 that means the player has fallen
     [SerializeField] private float maxRotation = 45; //how much the player will rotate when the playerRotation is 1 or -1 (eg 45 degrees)
 
     [Header("Falling thresholds")]
-    [SerializeField, Range(-1, 1)] private float fallThreshold = 0.9f; //the amount the player has to rotate before falling 
     [SerializeField, Range(0, 3)] private float speedThreshold = 0.3f; //the amount the player has to slow down to before falling
+    [SerializeField] bool godMode = false; //if true the player will not fall unless they are at the top of the pole
 
     private Transform playerPosition;  // The player's position
     private float playerStartPosY;  // The player's starting Y position
+    private float speed; // The player's speed
 
-    public bool IsIdle  { get; private set; } = true; 
+    public bool IsIdle { get; private set; } = true;
     public bool ReachedPole { get; private set; } = false;
     public bool IsFalling { get; private set; } = false;
 
     public float AdjustedPlayerPosY => Mathf.Max(0, PlayerPosY - playerStartPosY); // The player's adjusted Y position where the starting Y position is effectively 0
     private float PlayerPosY => playerPosition.position.y;  // The player's current Y position
+
+    public float Speed
+    {
+        get { return speed; }
+        set { speed = value < 0 ? 0 : value; }
+    }
 
     void Start()
     {
@@ -59,7 +65,7 @@ public class PlayerController : MonoBehaviour
         if (IsFalling || IsIdle) { return; }
 
         AdjustSpeedByHeight();
-        CheckThresholds();   
+        CheckSpeedThreshold();
     }
 
     /// <summary>
@@ -94,10 +100,11 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     public void Fall()
     {
+        if (IsFalling || (godMode && AdjustedPlayerPosY < 3.75f)) { return; }
         Speed = 0f;
         IsFalling = true;
         animator.enabled = false; //disable the animator component. which causes the player to become a ragdoll
-        PlayerFellEvent.Invoke();
+        PlayerFellEvent.Invoke(AdjustedPlayerPosY);
     }
 
     /// <summary>
@@ -110,14 +117,11 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// Checks if the player has lost balance or speed
+    /// Checks if the player has lost speed
     /// </summary>
-    private void CheckThresholds()
+    private void CheckSpeedThreshold()
     {
-        bool lostBalance = playerRotation > fallThreshold || playerRotation < -fallThreshold;
-        bool lostSpeed = Speed <= speedThreshold;
-        
-        if (lostBalance || lostSpeed)
+        if (Speed <= speedThreshold)
         {
             Fall();
         }
